0be72e48e88a5f53bf56f95438867d9e6324a073
Ariful Islam <arif98741@gmail.com>
2018-09-05 15:22:50 +0600

    Changes

--- jquery/Tutorial 5/js/main.js
+++ jquery/Tutorial 5/js/main.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
-  var x = 0;
-  // $('#btn').dblclick(function(){
-  //   x++
-  //   console.log("x is "+x);
-  // });
+  var x = " consectetur repellat dicta neque dignissimos eaque accusamus quibusdam pariatur.repellat dicta neque dignissimos eaque accusamus quibusdam pariatur.repellat dicta neque dignissimos eaque accusamus quibusdam pariatur.repellat dicta neque dignissimos eaque accusamus quibusdam pariatur. Consequuntur, eos dolores et.";
+  var i = 0;
+  /*setInterval(function(){
+    $('#message').append(x.charAt(i));
+    i++;
+    if(i> x.length+5){
+      i = 0 ;
+      $('#message').html('');
+    }
+
+  },80);*/
 
 
-  $('#inp').mouseleave(function(){
-      x++;
-      console.log(x);
-  });
+  var x = ['Arif','Shamim','Khayrul','Mehedi','Idris','Khan Hasan'];
+  setInterval(function(){
+    $('#message').html(x[i]);
+    i++;
+    if(i > x.length){
+      i = 0;
+    }
 
-
-
-
-
+  },400);
 
 });
