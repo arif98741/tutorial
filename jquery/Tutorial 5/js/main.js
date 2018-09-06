d16dcabc79dd95407bbabe8d5241121e03aeb7e0
Ariful Islam <arif98741@gmail.com>
2018-09-07 02:02:58 +0600

    head changed

--- jquery/Tutorial 5/js/main.js
+++ jquery/Tutorial 5/js/main.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-  var x = 0;
-  // $('#btn').dblclick(function(){
-  //   x++
-  //   console.log("x is "+x);
-  // });
-
-
-  $('#inp').mouseleave(function(){
-      x++;
-      console.log(x);
-  });
-
-
-
-
-
+  
 
 });
