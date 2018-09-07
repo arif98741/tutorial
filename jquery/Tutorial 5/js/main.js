23c471b1a9f154a33e621085b50be415d74faad7
Ariful Islam <arif98741@gmail.com>
2018-09-07 19:44:08 +0600

    Tutorial 6 Uploaded

--- jquery/Tutorial 5/js/main.js
+++ jquery/Tutorial 5/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+  var x = 0;
+  // $('#btn').dblclick(function(){
+  //   x++
+  //   console.log("x is "+x);
+  // });
+
+
+  $('#inp').mouseleave(function(){
+      x++;
+      console.log(x);
+  });
+
+
+
+
 
 
 });
---
+++ jquery/Tutorial 6/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  //show
+  /*$('#message').hide(5000,function(){
+    alert('yes i am shown');
+  });*/
+
+  //show
+  /*$('#message').show(5000,function(){
+    alert('yes i am shown');
+  });*/
+
+  //slideUp
+  //$('#message').slideUp(3000);
+
+  //slideDown
+  //$('#message').slideDown(500);
+
+
+  //slideToggle
+  //$('#message').slideToggle(500);
+
+  //using button
+  /*$('#btn').click(function(){
+      $('img').slideToggle(1000);
+    $('#message').slideToggle(1000);
+  })*/
+  //fadeIn
+  $('#message').fadeOut(1000);
+  $('img').fadeOut(1000);
+
+  //fadeOut
+  
+
+
+
+
+
+
+
+
+  //
+
+
+
+
+});
