0dd5a4e9edca3cfb235ca2a3660059ab8837b4fc
Ariful Islam <arif98741@gmail.com>
2018-09-17 01:01:18 +0600

    Tutorial 10 updated

---
+++ jquery/Tutorial 9/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  //Tutorial  9 - Jquery GET
+  $('#btn').click(function(){
+    //var d = $('#data').html();
+    //var c = $('#data').attr('data');
+    var i = $('#inp').val();
+
+    console.log(i);
+  });
+});
