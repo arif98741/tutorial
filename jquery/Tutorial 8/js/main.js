40c3e865e77af99d948b1c76f430a324d3c07777
Ariful Islam <arif98741@gmail.com>
2018-09-17 00:25:48 +0600

    Tutorial 8 Jquery SEt

--- jquery/Tutorial 8/js/main.js
+++ jquery/Tutorial 8/js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-  //Tutorial  8 - Callback Function
+  //Tutorial  8 - Jquery Set
+  $('#message').html('I am fine');
+  $('#btn').click(function(){
+    $('#inp').attr('placeholder','Enter Your Name');
+    $('#inp').val('Jhon');
 
+  });
 });
